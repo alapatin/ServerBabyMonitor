69fc9e44c04d7f469a457edbdb695a1810401173
Aleksandr Lapatin <a.lapatin@icloud.com>
2018-12-30 18:50:58 +0300

    Initial Commit

---
+++ ServerSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ServerSample
+//
+//  Created by macbook air on 30/12/2018.
+//  Copyright © 2018 a.lapatin@icloud.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
